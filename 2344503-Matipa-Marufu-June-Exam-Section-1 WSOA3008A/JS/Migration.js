6bd19a302c8d06bf4eb000e1579efa0947328797
Matipa Marufu <2344503@students.wits.ac.za>
2022-07-03 08:12:28 +0200

    Finished Website

--- 2344503-Matipa-Marufu-June-Exam-Section-1 WSOA3008A/JS/Migration.js
+++ 2344503-Matipa-Marufu-June-Exam-Section-1 WSOA3008A/JS/Migration.js
@@ -1,6 +1,6 @@
 
 function MigrationError()
 {
-    alert();
+    alert("Migration is the process of transferring content from old technologies to newer ones. Migration involves copying the work from the original machine and pasting it into the new environment. It is considered a much cheaper and easier option than running outdated software in a new environment. (Rubio and Wharton, 2020). Migration is challenging because of how relational digital artwork is. A digital piece is created with software, hardware and code requirements being met. When that piece is moved to a different environment, the software and/or the hardware may change and the new machine may not be able to read the old code.");
     setTimeout( location.href = 'Original.html', 2000);
 }
